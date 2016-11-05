ff5ee70c0cd92eb22fa6a6e8f98a5a8acd96f21f
antonxuanquang <antonxuanquang@yahoo.com>
2016-11-05 11:19:34 -0500

    create model

---
+++ models/serviceModel.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var service = new Schema({
+	name:  String,
+	address: String,
+	phone:   String,
+	hour: String,
+	website: String,
+	description: String
+	}
+});
+
+var Service = mongoose.model('Service', service);
+
+module.exports = Dishes;
